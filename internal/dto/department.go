cc60073f465d59989321f23d511e72bf83db72c6
shmily <3030681608@qq.com>
2025-12-24 10:54:12 +0800

    部门信息代码

---
+++ internal/dto/department.go
@@ -0,0 +1,28 @@
+package dto
+
+import "schedule_server/internal/model"
+
+// DepartmentItem 部门信息项
+type DepartmentItem struct {
+	DeptID   int64  `json:"dept_id"`
+	Name     string `json:"name"`
+	ParentID int64  `json:"parent_id"`
+}
+
+// DepartmentListResponse 部门列表响应
+type DepartmentListResponse struct {
+	Items []DepartmentItem `json:"items"`
+}
+
+// NewDepartmentListResponse 从 model.Department 切片构造响应
+func NewDepartmentListResponse(depts []model.Department) *DepartmentListResponse {
+	items := make([]DepartmentItem, 0, len(depts))
+	for _, d := range depts {
+		items = append(items, DepartmentItem{
+			DeptID:   d.DeptID,
+			Name:     d.Name,
+			ParentID: d.ParentID,
+		})
+	}
+	return &DepartmentListResponse{Items: items}
+}
