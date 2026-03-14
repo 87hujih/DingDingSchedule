4c7b68f633f671a5408a36ca11f19fab230278e2
shmily <3030681608@qq.com>
2026-03-14 11:25:52 +0800

    bug修改

--- internal/model/group_attendance_subscription.go
+++ internal/model/group_attendance_subscription.go
@@ -8,9 +8,9 @@ import (
 
 // GroupAttendanceSubscription 群考勤推送订阅
 type GroupAttendanceSubscription struct {
-	ID             uint           `gorm:"primaryKey"`
-	TenantID       uint           `gorm:"not null;uniqueIndex:uniq_tenant_conv"`
-	ConversationID string         `gorm:"not null;uniqueIndex:uniq_tenant_conv"`
+	ID             uint   `gorm:"primaryKey"`
+	TenantID       uint   `gorm:"not null;uniqueIndex:uniq_tenant_conv"`
+	ConversationID string `gorm:"not null;size:191;uniqueIndex:uniq_tenant_conv"`
 	GroupName      string
 	EnabledByUID   uint
 	CreatedAt      time.Time
